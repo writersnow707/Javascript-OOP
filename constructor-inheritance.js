67172560b15e70fcadc872315f5e272a13b89b9e
snow707 <writersnow707@gmail.com>
2022-11-17 00:14:07 +0900

    211117 12:14 modified

--- constructor-inheritance.js
+++ constructor-inheritance.js
@@ -11,10 +11,14 @@ function PersonPlus(name, first, second, third) {
     Person.call(this, name, first, second); // => super(name, firsts, second)
     this.third = third;
 }
-PersonPlus.prototype.__proto__ = Person.prototype;
+// PersonPlus.prototype.__proto__ = Person.prototype;
+PersonPlus.prototype = Object.create(Person.prototype);
+PersonPlus.prototype.constructor = PersonPlus;
+
 PersonPlus.prototype.avg = function() {
     return (this.first+this.second+this.third)/3;
 }
 let kim = new PersonPlus('kim', 10, 20, 30);
 console.log("kim.sum()", kim.sum());
 console.log("kim.avg()", kim.avg());
+console.log("kim.constructor", kim.constructor);
